08cfad9766f9b1ccf76ac4a0ea56c3f665f31a40
Paolo T. Inocencion <paoloinocencion@gmail.com>
2017-07-22 20:34:53 -0400

    Started project.

---
+++ src/Solution.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    static int[] getRecord(int[] s){
+        // Complete this function
+
+
+
+        return s;
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int[] s = new int[n];
+        for(int s_i=0; s_i < n; s_i++){
+            s[s_i] = in.nextInt();
+        }
+        int[] result = getRecord(s);
+        String separator = "", delimiter = " ";
+        for (Integer val : result) {
+            System.out.print(separator + val);
+            separator = delimiter;
+        }
+        System.out.println("");
+    }
+}
